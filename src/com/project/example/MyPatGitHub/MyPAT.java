75ac33942528347318ce2064c77c74193d69975c
rajesh.kumar1 <rajesh.kumar1@LTPBAN292475766.corp.mphasis.com>
2023-11-05 22:54:20 +0530

    removing

--- src/com/project/example/MyPatGitHub/MyPAT.java
+++
@@ -1,9 +0,0 @@
-package com.project.example.MyPatGitHub;
-
-public class MyPAT {
-
-	public static void main(String[] args) {
-		System.out.println("My PAT Github");
-		System.out.println("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");
-	}
-}
