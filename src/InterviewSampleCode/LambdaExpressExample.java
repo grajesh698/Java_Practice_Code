fef21a11756181ab7b27011f482205766c3c768e
rajesh.kumar1 <rajesh.kumar1@LTPBAN292475766.corp.mphasis.com>
2023-11-17 17:05:15 +0530

    Lambda expression

---
+++ src/InterviewSampleCode/LambdaExpressExample.java
@@ -0,0 +1,18 @@
+package InterviewSampleCode;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class LambdaExpressExample {
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(5);
+		numbers.add(9);
+		numbers.add(8);
+		numbers.add(1);
+		Consumer<Integer> method = (n) -> {
+			System.out.print(n+" ");
+		};
+		numbers.forEach(method);
+	}
+}
