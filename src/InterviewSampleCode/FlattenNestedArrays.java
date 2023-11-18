ed9aee05234e991d7ba8abf43a08e9a209d9efe5
R_Kalwaar <R_Kalwaar@DESKTOP-24848MP>
2023-11-18 17:26:41 +0530

    Update solutions

--- src/InterviewSampleCode/FlattenNestedArrays.java
+++ src/InterviewSampleCode/FlattenNestedArrays.java
@@ -1,19 +1,43 @@
 package InterviewSampleCode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class FlattenNestedArrays {
 	public static void main(String[] args) throws Exception {
 		Object[] array = { 1, 2, new Object[] { 3, 4, new Object[] { 5 }, 6, 7 }, 8, 9, 10 };
-		Object[] flattenedArray = flatten(array).toArray();
 
-		System.out.println(Arrays.toString(flattenedArray));
+		System.out.println(Arrays.toString(flatten(array).toArray()));
+
+		System.out.println("----------------Second Approach for Integer Input----------------");
+
+		System.out.println(Arrays.toString(flatten2(array)));
 
 	}
 
 	public static Stream<Object> flatten(Object[] inputArray) {
 
 		return Arrays.stream(inputArray).flatMap(o -> o instanceof Object[] a ? flatten(a) : Stream.of(o));
+	}
+
+	// -------------------Second Approach for Integer Input--------------------------------//
+
+	public static Integer[] flatten2(Object[] inputArray) throws Exception {
+
+		if (inputArray == null)
+			return null;
+
+		List<Integer> list = new ArrayList<Integer>();
+
+		for (Object element : inputArray) {
+			if (element instanceof Integer) {
+				list.add((Integer) element);
+			} else {
+				list.addAll(Arrays.asList(flatten2((Object[]) element)));
+			}
+		}
+		return list.toArray(new Integer[list.size()]);
 	}
 }
--- src/InterviewSampleCode/splitArraytoSpecificSize.java
+++ src/InterviewSampleCode/splitArraytoSpecificSize.java
@@ -1,10 +1,6 @@
 package InterviewSampleCode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
-import org.apache.commons.lang.ArrayUtils;
 
 /* 
 Split the Array in given splitSize
@@ -20,32 +16,10 @@ public class splitArraytoSpecificSize {
 	public static void main(String[] args) {
 		int[] original = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		int splitSize = 3;
-
-		List<Integer[]> arrayParts = splitArray(ArrayUtils.toObject(original), splitSize);
-		for (Integer[] o : arrayParts) {
-			System.out.println(Arrays.toString(o));
+		for (int i = 0; i < original.length; i += splitSize) {
+			System.out.println(
+					Arrays.toString(Arrays.copyOfRange(original, i, Math.min(original.length, i + splitSize))));
 		}
 
-	}
-
-	public static <T> List<T[]> splitArray(T[] array, int splitSize) {
-
-		int numberOfArrays = array.length / splitSize;
-		int remainder = array.length % splitSize;
-
-		int start = 0;
-		int end = 0;
-
-		List<T[]> list = new ArrayList<T[]>();
-		for (int i = 0; i < numberOfArrays; i++) {
-			end += splitSize;
-			list.add(Arrays.copyOfRange(array, start, end));
-			start = end;
-		}
-
-		if (remainder > 0) {
-			list.add(Arrays.copyOfRange(array, start, (start + remainder)));
-		}
-		return list;
 	}
 }
