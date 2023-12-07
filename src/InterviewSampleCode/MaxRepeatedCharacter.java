4f296a585a95c2fe3778be28e6c91ac6f72ec76a
rajesh.kumar1 <rajesh.kumar1@LTPBAN292475766.corp.mphasis.com>
2023-12-07 21:27:55 +0530

    Updating code.

--- src/InterviewSampleCode/MaxRepeatedCharacter.java
+++ src/InterviewSampleCode/MaxRepeatedCharacter.java
@@ -6,17 +6,21 @@ import java.util.Map;
 public class MaxRepeatedCharacter {
 	public static void main(String[] args) {
 		String inputString = "aaabbbcbccabbdaa";
-		findMaxRepeatedCharacter(inputString);
+		if (inputString.length() == 0) {
+			System.out.println("String is empty!");
+		} else {
+			findMaxRepeatedCharacter(inputString);
+		}
 	}
 
 	private static void findMaxRepeatedCharacter(String str) {
 		Map<Character, Integer> charCount = new HashMap<>();
 		int maxCount = 0;
-		
+
 		for (char c : str.toCharArray()) {
 			charCount.put(c, charCount.getOrDefault(c, 0) + 1);
 		}
-		
+
 		for (int count : charCount.values()) {
 			maxCount = Math.max(maxCount, count);
 		}
